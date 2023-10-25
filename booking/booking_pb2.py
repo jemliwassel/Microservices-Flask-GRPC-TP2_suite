5cac586c07b2fe5449d6a5d8e62a58c9010e90ce
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2023-10-26 01:06:48 +0200

    feat: add booking_pb2.py file

--- booking/booking_pb2.py
+++ booking/booking_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rbooking.proto\"\x19\n\x06UserID\x12\x0f\n\x07user_id\x18\x01 \x01(\t\":\n\x0b\x42ookingData\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x1a\n\x05\x64\x61tes\x18\x02 \x03(\x0b\x32\x0b.MovieDates\"*\n\nMovieDates\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0e\n\x06movies\x18\x02 \x03(\t\"D\n\x11\x41\x64\x64\x42ookingRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61te\x18\x02 \x01(\t\x12\x10\n\x08movie_id\x18\x03 \x01(\t\"6\n\x12\x41\x64\x64\x42ookingResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x0f\n\x07message\x18\x02 \x01(\t2x\n\x07\x42ooking\x12-\n\x12GetBookingByUserID\x12\x07.UserID\x1a\x0c.BookingData\"\x00\x12>\n\x11\x41\x64\x64\x42ookingForUser\x12\x12.AddBookingRequest\x1a\x13.AddBookingResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rbooking.proto\"\x19\n\x06UserID\x12\x0f\n\x07user_id\x18\x01 \x01(\t\":\n\x0b\x42ookingData\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x1a\n\x05\x64\x61tes\x18\x02 \x03(\x0b\x32\x0b.MovieDates\"*\n\nMovieDates\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0e\n\x06movies\x18\x02 \x03(\t\"D\n\x11\x41\x64\x64\x42ookingRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61te\x18\x02 \x01(\t\x12\x10\n\x08movie_id\x18\x03 \x01(\t\"\x0e\n\x0c\x45mptyBooking2\xa7\x01\n\x07\x42ooking\x12/\n\x12GetBookingByUserID\x12\x07.UserID\x1a\x0c.BookingData\"\x00\x30\x01\x12\x37\n\x11\x41\x64\x64\x42ookingForUser\x12\x12.AddBookingRequest\x1a\x0c.BookingData\"\x00\x12\x32\n\x0fGetListBookings\x12\r.EmptyBooking\x1a\x0c.BookingData\"\x00\x30\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -29,8 +29,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_MOVIEDATES']._serialized_end=146
   _globals['_ADDBOOKINGREQUEST']._serialized_start=148
   _globals['_ADDBOOKINGREQUEST']._serialized_end=216
-  _globals['_ADDBOOKINGRESPONSE']._serialized_start=218
-  _globals['_ADDBOOKINGRESPONSE']._serialized_end=272
-  _globals['_BOOKING']._serialized_start=274
-  _globals['_BOOKING']._serialized_end=394
+  _globals['_EMPTYBOOKING']._serialized_start=218
+  _globals['_EMPTYBOOKING']._serialized_end=232
+  _globals['_BOOKING']._serialized_start=235
+  _globals['_BOOKING']._serialized_end=402
 # @@protoc_insertion_point(module_scope)
