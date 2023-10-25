b8a66b808fa62bcb128754f44b99439db0e88afa
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2023-10-26 01:06:24 +0200

    feat: add functions to test the Booking service

--- booking/client.py
+++ booking/client.py
@@ -3,36 +3,47 @@ import grpc
 import booking_pb2
 import booking_pb2_grpc
 
+
+def get_list_booking(stub):
+    bookings = stub.GetListBookings(booking_pb2.EmptyBooking())
+    for booking in bookings:
+        print(booking)
+
+
 def get_booking_by_user_id(stub, user_id):
-    user_booking = stub.GetBookingByUserID(user_id)
-    print("Booking : " , user_booking)
+    user_bookings = stub.GetBookingByUserID(user_id)
+    for booking in user_bookings:
+        print(booking)
+
 
 def add_booking_for_user(stub, user_id, date, movie_id):
-    booking_request = booking_pb2.AddBookingRequest(user_id=user_id, date=date, movie_id=movie_id)
+    booking_request = booking_pb2.AddBookingRequest(
+        user_id=user_id, date=date, movie_id=movie_id
+    )
     response = stub.AddBookingForUser(booking_request)
-    if response.success:
-        print(response.message)
-    else:
-        print(f"Échec de la réservation : {response.message}")
-        
+    print(response)
+
+
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:3001') as channel:
+    with grpc.insecure_channel("localhost:3001") as channel:
         stub = booking_pb2_grpc.BookingStub(channel)
+        print("-------------- GetListBooking --------------")
+        get_list_booking(stub)
         print("------------------- GetBookingByUserID----------------")
-        user_id = booking_pb2.UserID(user_id  = "chris_rivers")
+        user_id = booking_pb2.UserID(user_id="chris_rivers")
         get_booking_by_user_id(stub, user_id)
         print("-----------------AddBookingForUser----------------------")
-        # booking_request = booking_pb2.AddBookingRequest(
-        #     user_id= "chris_rivers",
-        #     date = "20151201",
-        #     movie_id ="267eedb8-0f5d-42d5-8f43-72426b9fb3e6"
-        # )
-        add_booking_for_user(stub, user_id= "chris_rivers", date = "20151201", movie_id ="FR2023HORROR")
+        add_booking_for_user(
+            stub,
+            user_id="chris_rivers",
+            date="20151201",
+            movie_id="7daf7208-be4d-4944-a3ae-c1c2f516f3e6",
+        )
     channel.close()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     run()
-    
